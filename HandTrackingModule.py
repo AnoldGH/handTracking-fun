14c7172b573551f81b8ff54e88aa2b5b875f6027
Haotian Yi <yi2021@g.ucla.edu>
2023-10-08 17:11:05 -0700

    Added several methods for a drawing sub-module of hand detector

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -19,6 +19,13 @@ class handDetector():
         self.mpDraw = mp.solutions.drawing_utils
         self.results = None
         
+        # Drawing Module
+        self.drawings = list()
+        
+        # todo: parametrize the following
+        self.height = 360
+        self.width = 640
+        
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
@@ -46,6 +53,38 @@ class handDetector():
         
         return landmarks
     
+    def update(self, img):
+        self.results = self.hands.process(img, cv2.COLOR_BGR2RGB)
+    
+    # Drawing Sub-module
+    def _track_landmark_safe(self, img, hdID, lmID, radius, color, thickness=1, linetype=cv2.LINE_8):
+        if self.results.multi_hand_landmarks:
+            hand = self.results.multi_hand_landmarks[hdID]
+            lm = hand.landmark[lmID]
+            cx, cy = int(self.width * lm.x), int(self.height * lm.y)
+            cv2.circle(img, (cx, cy), radius, color, thickness, linetype)
+    
+    def _connect_landmarks_safe(self, img, hdID, lm1ID, lm2ID, color, thickness=1, linetype=cv2.LINE_8):
+        if self.results.multi_hand_landmarks:
+            hand = self.results.multi_hand_landmarks[hdID]
+            lm1, lm2 = hand[lm1ID], hand[lm2ID]
+            cv2.line(img, (lm1.x, lm1.y), (lm2.x, lm2.y), \
+                color, thickness, linetype)
+    
+    def track_landmark(self, img, hdID, lmID, radius, color, thickness=1, linetype=cv2.LINE_8):
+        curry = lambda: self._track_landmark_safe(img, hdID, lmID, radius, color, thickness, linetype)
+        self.drawings.append(curry)    
+        
+    def connect_landmarks(self, img, hdID, lm1ID, lm2ID, color, thickness=1, linetype=cv2.LINE_8):
+        curry = lambda: \
+            self._connect_landmarks_safe(img, hdID, lm1ID, lm2ID, color, thickness, linetype)
+        self.drawings.append(curry)
+    
+    def render(self):
+        for drawing in self.drawings:
+            drawing()
+    
+    
 def main():
     pTime = 0
     cTime = 0
@@ -55,11 +94,10 @@ def main():
     video = cv2.VideoWriter('video.mp4', -1, 24, (640, 360))
     
     i = 0
-    while i < 150:
+    while True:
         success, img = capture.read()
         if success:
             img = detector.findHands(img)
-            print(img.shape)
             
             cTime = time.time()
             fps = 1 / (cTime - pTime)
@@ -67,8 +105,13 @@ def main():
             
             cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
             
+            # video.write(img)
+            
+            # Drawing
+            detector.track_landmark(img, 0, 4, 15, (0, 255, 255))
+            detector.render()
+            
             cv2.imshow("Image", img)
-            video.write(img)
             
             cv2.waitKey(1)
         else:
